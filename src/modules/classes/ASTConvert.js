cf074ad83f482fcef47103b0aca47602f32eac55
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-07-29 21:55:07 +0200

    Usage "addAttributes" method where it is needed

--- src/modules/classes/ASTConvert.js
+++ src/modules/classes/ASTConvert.js
@@ -20,43 +20,7 @@ class AST{
         //Attributes handling
         const attributes = obj.attributes;
 
-        //To this object will go all attributes, it is object cuz I will use object.assing in order to add these attributes to element - https://stackoverflow.com/questions/12274748/setting-multiple-attributes-for-an-element-at-once-with-javascript
-        const attributesObj = {};
-    
-        //String where will be all names of classes(there can be a lot of classes, so this app creates one string with all classes)
-        let valueContentClass = "";
-        
-        for(const attribut of attributes){
-          if(attribut.name === "class"){
-            //Must be className, not class - https://developer.mozilla.org/pl/docs/Web/API/Element/className
-            attribut.name = "className";
-    
-            //There can be a lot of classes, this code below "combines" class names into one string(later this one big string will be add as className)
-            if(attributesObj.className){
-              valueContentClass = valueContentClass+" "+attribut.value;
-            }else{
-              valueContentClass = attribut.value;
-            }
-    
-            //Define object properties
-            Object.defineProperty(attributesObj, attribut.name, {
-              value: valueContentClass,
-              configurable: true,
-              //It must be true in order to object assing will able "see" it - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty#description
-              enumerable: true,
-            });
-          }else{
-            //Define properties if it is not class
-            Object.defineProperty(attributesObj, attribut.name, {
-              value: attribut.value,
-              configurable: true,
-              //It must be true in order to object assing will able "see" it - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty#description
-              enumerable: true,
-            });
-          }
-        }
-        //Add attributes to element - https://stackoverflow.com/questions/12274748/setting-multiple-attributes-for-an-element-at-once-with-javascript
-        Object.assign(element, attributesObj);
+        this.addAttributes(element, attributes);
         
         this.mainElement = element;
     }
@@ -67,7 +31,10 @@ class AST{
       //If there is something like const [childElement] of Object.entries(children)), the childElement takes over the roles of index
       for(const [key, childElement] of Object.entries(children)){
         if(childElement.nodeType === "element"){
+          //Create an element and add suitable attributes
           const element = document.createElement(childElement.tagName);
+          this.addAttributes(element, childElement.attributes);
+
           parentElement.appendChild(element);
 
           //This condition provides that this method will not call indefinitely
