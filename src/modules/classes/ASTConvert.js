e60c116667b09eba28dc5dce2ae02c3aa4f6e6c0
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-07-29 21:48:51 +0200

    Create new method which adds attributes to element
    This method is responsible for adding attributes to elements, it is a
    copy from createMainElement method, there is one chnage, in this method
    I do not create an element based on tagName(first two lines in createMainElement), cuz I have this element in
    method as an argument

--- src/modules/classes/ASTConvert.js
+++ src/modules/classes/ASTConvert.js
@@ -68,7 +68,6 @@ class AST{
       for(const [key, childElement] of Object.entries(children)){
         if(childElement.nodeType === "element"){
           const element = document.createElement(childElement.tagName);
-
           parentElement.appendChild(element);
 
           //This condition provides that this method will not call indefinitely
@@ -81,6 +80,46 @@ class AST{
           parentElement.innerHTML = childElement.value;
         }
       }
+    }
+
+    addAttributes(element, attributesArray){
+      //To this object will go all attributes, it is object cuz I will use object.assing in order to add these attributes to element - https://stackoverflow.com/questions/12274748/setting-multiple-attributes-for-an-element-at-once-with-javascript
+      const attributesObj = {};
+  
+      //String where will be all names of classes(there can be a lot of classes, so this app creates one string with all classes)
+      let valueContentClass = "";
+      
+      for(const attribut of attributesArray){
+        if(attribut.name === "class"){
+          //Must be className, not class - https://developer.mozilla.org/pl/docs/Web/API/Element/className
+          attribut.name = "className";
+  
+          //There can be a lot of classes, this code below "combines" class names into one string(later this one big string will be add as className)
+          if(attributesObj.className){
+            valueContentClass = valueContentClass+" "+attribut.value;
+          }else{
+            valueContentClass = attribut.value;
+          }
+  
+          //Define object properties
+          Object.defineProperty(attributesObj, attribut.name, {
+            value: valueContentClass,
+            configurable: true,
+            //It must be true in order to object assing will able "see" it - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty#description
+            enumerable: true,
+          });
+        }else{
+          //Define properties if it is not class
+          Object.defineProperty(attributesObj, attribut.name, {
+            value: attribut.value,
+            configurable: true,
+            //It must be true in order to object assing will able "see" it - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty#description
+            enumerable: true,
+          });
+        }
+      }
+      //Add attributes to element - https://stackoverflow.com/questions/12274748/setting-multiple-attributes-for-an-element-at-once-with-javascript
+      Object.assign(element, attributesObj);
     }
 }
 
